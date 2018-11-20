3ee760d8ee131af9f853eec54c44e8202bd940ee
shufengw <imgamer@163.com>
2018-11-20 17:02:34 +0800

    init

---
+++ assets/scripts/kbengine/Account.ts
@@ -0,0 +1,38 @@
+
+import GameEntity from "./GameEntity";
+import KBEDebug from "../../kbengine_typescript_plugin/kbengine/KBEDebug";
+import * as Datatypes from "../../kbengine_typescript_plugin/kbengine/DataTypes";
+
+
+export default class Account extends GameEntity
+{
+    static SCRIPT_NAME = "Account";
+
+    set_dbidFromClient(oldval: any)
+    {
+        KBEDebug.INFO_MSG("Entity::set_dbidFromClient------------------->>>id:%d value:%s.", this.id, this["dbidFromClient"].toString());
+        
+    }
+
+    set_wxnickname(oldval: any)
+    {
+        KBEDebug.INFO_MSG("Entity::set_wxnickname------------------->>>id:%d value:%s.", this.id, this["wxnickname"].toString());
+
+        let data = Datatypes.BuildUINT64(15);
+        this.BaseCall("pingBase", data);
+    }
+
+    pingBack(data: Datatypes.UINT64)
+    {
+        KBEDebug.INFO_MSG("Entity::pingBack------------------->>>id:%d value:%s.", this.id, data.toString());
+    }
+    
+    receiveLoginAffiche(LOGIN_AFFICHE_DATA)
+    {
+        let title = LOGIN_AFFICHE_DATA["title"];
+        let msg = LOGIN_AFFICHE_DATA["msg"];
+        KBEDebug.INFO_MSG("Entity::receiveLoginAffiche------------------->>>title:%s.msg:%s.", title, msg);
+    }
+}
+
+Account.RegisterScript(Account.SCRIPT_NAME, Account);
---
+++ assets/scripts/kbengine/GameEntity.ts
@@ -0,0 +1,10 @@
+
+import Entity from "../../kbengine_typescript_plugin/kbengine/Entity";
+
+export default class GameEntity extends Entity
+{
+    constructor()
+    {
+        super();
+    }
+}
